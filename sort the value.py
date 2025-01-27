25f9cb3b8d27a76b66c7b49aa965edcd84359c9e
DILLI PRASATH B <147839691+dilliprasathB@users.noreply.github.com>
2025-01-27 19:18:58 +0530

    Add files via upload
    sort the value as 0s,1s,2s

---
+++ sort the value.py
@@ -0,0 +1,7 @@
+'''Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+
+You need to solve this problem without utilizing the built-in sort function.'''
+
+def sort012 (arr):
+    arr.sort()
+    return arr
