8d123609ee0628988f4ffed5c8d5304b3b2d1677
DILLI PRASATH B <147839691+dilliprasathB@users.noreply.github.com>
2025-01-28 18:14:14 +0530

    Add files via upload

---
+++ rotated array by one.py
@@ -0,0 +1,16 @@
+#rotated array by one
+
+def rotatedbyone (arr):
+    n=len(arr)
+    i,j=0,n-2
+    while i<j:
+        arr[i],arr[j]=arr[j],arr[i]
+        i=+1
+        j-=1
+
+    i,j=o,n-1
+    while i<j:
+        arr[i],arr[j]=arr[j],arr[i]
+        i+=1
+        j-=1
+
