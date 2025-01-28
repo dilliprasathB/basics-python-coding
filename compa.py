85f5447ffdbbaa26932e5d4c17914aa60d6a165e
DILLI PRASATH B <147839691+dilliprasathB@users.noreply.github.com>
2025-01-28 17:29:47 +0530

    Add files via upload

---
+++ compa.py
@@ -0,0 +1,9 @@
+letter1="a"
+letter2="b"
+
+print(f"'{letter1}'<'{letter2}' is {letter1 < letter2}")
+print(f"'{letter1}'<='{letter2}' is {letter1 <= letter2}")
+print(f"'{letter1}'>'{letter2}' is {letter1 > letter2}")
+print(f"'{letter1}'>='{letter2}' is {letter1 >= letter2}")
+print(f"'{letter1}'=='{letter2}' is {letter1 == letter2}")
+print(f"'{letter1}'!='{letter2}' is {letter1 != letter2}")
